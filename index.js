57abad1ba16ff192a6536893117d20b25d715af4
Youssef Hassan Lotfy <ejrgfjg@gmail.com>
2025-12-01 20:11:52 +0200

    edited sensordata

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('Connection ERROR with MongoDB:', err);
     dbConnected = false; // stay false if failed
 });
-
-
   
 
 // Homepage route
